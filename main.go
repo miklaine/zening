a9458259a19ab9a379cb8a41b4721d374875e6a2
miklaine <mishapyan@gmail.com>
2023-12-04 18:13:34 -0500

    add basic structure

--- main.go
+++ main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type InputJSON struct {
+	Data map[string]interface{} `json:"-"`
+}
+
+func parseInputJSON(filename string) (*InputJSON, error) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &InputJSON{Data: data}, nil
+}
+
+func main() {
+	// Assume the input JSON file is provided as the first command-line argument
+	inputFile := os.Args[1]
+	inputJSON, err := parseInputJSON(inputFile)
+	if err != nil {
+		fmt.Println("Error parsing input JSON:", err)
+		return
+	}
+
+	// TODO: Implement the transformation logic
+
+	fmt.Println(inputJSON)
+}
